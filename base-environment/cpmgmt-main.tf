f9cb93f7b750c067de8fb4a04fe6730e32f6b581
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-07-24 22:30:47 +0200

    improving code style

--- base-environment/cpmgmt-main.tf
+++ base-environment/cpmgmt-main.tf
@@ -1,27 +1,14 @@
-# Accept the agreement for the mgmt-byol for R80.40
-resource "azurerm_marketplace_agreement" "cpmgmt-agreement" {
-  count     = var.mgmt-sku-enabled ? 0 : 1
-  publisher = "checkpoint"
-  offer     = "check-point-cg-${var.mgmt-version}"
-  plan      = var.mgmt-sku
-}
-
-# Create management resource group
 resource "azurerm_resource_group" "rg-ckpmgmt" {
   name      = "rg-${var.mgmt-name}"
   location  = var.location
 }
-
-# Create NSG for the management
 resource "azurerm_network_security_group" "nsg-ckpmgmt" {
   name                = "nsg-${var.mgmt-name}"
-  location            = var.location
-  resource_group_name = "rg-${var.mgmt-name}"
-  depends_on = [azurerm_resource_group.rg-ckpmgmt]
+  location            = azurerm_resource_group.rg-ckpmgmt.location
+  resource_group_name = azurerm_resource_group.rg-ckpmgmt.name
 }
 
 # Create the NSG rules for the management
-
 resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-ssh" {
   priority  = 100
   name      = "ssh-access"
@@ -33,9 +20,8 @@ resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-ssh" {
   destination_port_range      = "22"
   source_address_prefix       = var.my-pub-ip
   destination_address_prefix  = "*"
-  resource_group_name         = "rg-${var.mgmt-name}"
-  network_security_group_name = "nsg-${var.mgmt-name}"
-  depends_on = [azurerm_network_security_group.nsg-ckpmgmt]
+  resource_group_name         = azurerm_resource_group.rg-ckpmgmt.name
+  network_security_group_name = azurerm_network_security_group.nsg-ckpmgmt.name
 }
 
 resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-https" {
@@ -49,9 +35,8 @@ resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-https" {
   destination_port_range      = "443"
   source_address_prefix       = var.my-pub-ip
   destination_address_prefix  = "*"
-  resource_group_name         = "rg-${var.mgmt-name}"
-  network_security_group_name = "nsg-${var.mgmt-name}"
-  depends_on = [azurerm_network_security_group.nsg-ckpmgmt]
+  resource_group_name         = azurerm_resource_group.rg-ckpmgmt.name
+  network_security_group_name = azurerm_network_security_group.nsg-ckpmgmt.name
 }
 
 resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-smartconsole" {
@@ -65,9 +50,8 @@ resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-smartconsole" {
   destination_port_ranges     = ["18190","19009"]
   source_address_prefix       = var.my-pub-ip
   destination_address_prefix  = "*"
-  resource_group_name         = "rg-${var.mgmt-name}"
-  network_security_group_name = "nsg-${var.mgmt-name}"
-  depends_on = [azurerm_network_security_group.nsg-ckpmgmt]
+  resource_group_name         = azurerm_resource_group.rg-ckpmgmt.name
+  network_security_group_name = azurerm_network_security_group.nsg-ckpmgmt.name
 }
 
 resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-exposedsrvc" {
@@ -81,26 +65,22 @@ resource "azurerm_network_security_rule" "nsg-ckpmgmt-rl-exposedsrvc" {
   destination_port_ranges     = ["257","18210","18264","18191"]
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = "rg-${var.mgmt-name}"
-  network_security_group_name = "nsg-${var.mgmt-name}"
-  depends_on = [azurerm_network_security_group.nsg-ckpmgmt]
+  resource_group_name         = azurerm_resource_group.rg-ckpmgmt.name
+  network_security_group_name = azurerm_network_security_group.nsg-ckpmgmt.name
 }
 
-# Create Public IP
 resource "azurerm_public_ip" "pub-ckpmgmt" {
   name                = "pub-${var.mgmt-name}"
-  location            = var.location
-  resource_group_name = "rg-${var.mgmt-name}"
+  location            = azurerm_resource_group.rg-ckpmgmt.location
+  resource_group_name = azurerm_resource_group.rg-ckpmgmt.name
   allocation_method   = "Dynamic"
   domain_name_label   = "pub-${var.mgmt-name}-${var.mgmt-dns-suffix}"
-  depends_on = [azurerm_resource_group.rg-ckpmgmt]
 }
 
-# Create NIC
 resource "azurerm_network_interface" "nic-ckpmgmt" {
   name                  = "${var.mgmt-name}-eth0"
-  location              = var.location
-  resource_group_name   = "rg-${var.mgmt-name}"
+  location              = azurerm_resource_group.rg-ckpmgmt.location
+  resource_group_name   = azurerm_resource_group.rg-ckpmgmt.name
   enable_ip_forwarding  = "false"
   
   ip_configuration {
@@ -110,30 +90,22 @@ resource "azurerm_network_interface" "nic-ckpmgmt" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.pub-ckpmgmt.id
   }
-  depends_on = [azurerm_public_ip.pub-ckpmgmt,azurerm_subnet.net-secmgmt,azurerm_network_security_group.nsg-ckpmgmt]
 }
-
 resource "azurerm_network_interface_security_group_association" "nsg-assoc-nic-ckpmgmt" {
   network_interface_id      = azurerm_network_interface.nic-ckpmgmt.id
   network_security_group_id = azurerm_network_security_group.nsg-ckpmgmt.id
-  depends_on = [azurerm_network_interface.nic-ckpmgmt,azurerm_network_security_group.nsg-ckpmgmt]
 }
 
-# Generate random text for a unique storage account name
 resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = "rg-${var.mgmt-name}"
-  }
-  byte_length = 8
+  byte_length = 4
+  keepers = { resource_group = azurerm_resource_group.rg-ckpmgmt.name }
   depends_on = [azurerm_resource_group.rg-ckpmgmt]
 }
 
-# Create storage account for boot diagnostics
 resource "azurerm_storage_account" "ckp-storageaccount" {
   name                      = "diag${random_id.randomId.hex}"
-  resource_group_name       = "rg-${var.mgmt-name}"
-  location                  = var.location
+  resource_group_name       = azurerm_resource_group.rg-ckpmgmt.name
+  location                  = azurerm_resource_group.rg-ckpmgmt.location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
   depends_on = [random_id.randomId,azurerm_resource_group.rg-ckpmgmt]
@@ -142,8 +114,8 @@ resource "azurerm_storage_account" "ckp-storageaccount" {
 # Create virtual machine
 resource "azurerm_virtual_machine" "ckpmgmt" {
   name                  = var.mgmt-name
-  location              = var.location
-  resource_group_name   = "rg-${var.mgmt-name}"
+  resource_group_name   = azurerm_resource_group.rg-ckpmgmt.name
+  location              = azurerm_resource_group.rg-ckpmgmt.location
   vm_size               = var.mgmt-size
   network_interface_ids = [azurerm_network_interface.nic-ckpmgmt.id]
   primary_network_interface_id = azurerm_network_interface.nic-ckpmgmt.id
@@ -180,10 +152,8 @@ resource "azurerm_virtual_machine" "ckpmgmt" {
     enabled     = "true"
     storage_uri = azurerm_storage_account.ckp-storageaccount.primary_blob_endpoint
   }
-  depends_on = [azurerm_marketplace_agreement.cpmgmt-agreement,azurerm_resource_group.rg-ckpmgmt,azurerm_network_interface.nic-ckpmgmt,azurerm_storage_account.ckp-storageaccount]
 }
 
 output "mgmt-output-fqdn" {
   value = azurerm_public_ip.pub-ckpmgmt.fqdn
-  depends_on = [azurerm_public_ip.pub-ckpmgmt]
 }
